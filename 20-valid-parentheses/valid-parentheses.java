bdbec95d1bc88c729a20b2936686b5e706348ee7
Kaushal Raj <kaushalrajverma2007@gmail.com>
2026-05-01 00:21:52 +0530

    Time: 5 ms (15.55%) | Memory: 43.4 MB (26.37%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+        int len = s.length();
+
+        for (int i = 0; i < len; i++) {
+            char ch = s.charAt(i);
+            if (stk.isEmpty()) {
+                stk.push(ch);
+                continue;
+            }
+            if (stk.peek() == '{' && ch == '}')
+                stk.pop();
+            else if (stk.peek() == '(' && ch == ')')
+                stk.pop();
+            else if (stk.peek() == '[' && ch == ']')
+                stk.pop();
+            else
+                stk.push(ch);
+        }
+        if (stk.isEmpty())
+            return true;
+        else
+            return false;
+
+    }
+}
