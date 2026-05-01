e5ad79c65d7c4022271ce04868e5a4f95c3bcf8f
Kaushal Raj <kaushalrajverma2007@gmail.com>
2026-05-01 21:56:52 +0530

    Time: 2 ms (98.04%) | Memory: 43.4 MB (41.05%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,29 +1,45 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stk = new Stack<>();
-        int len = s.length();
+        // int len = s.length();
 
-        for (int i = 0; i < len; i++) {
-            char ch = s.charAt(i);
-            if (stk.isEmpty()) {
-                stk.push(ch);
-                continue;
+        // for (int i = 0; i < len; i++) {
+        //     char ch = s.charAt(i);
+        //     if (stk.isEmpty()) {
+        //         stk.push(ch);
+        //         continue;
+        //     }
+        //     if (stk.peek() == '{' && ch == '}')
+        //         stk.pop();
+        //     else if (stk.peek() == '(' && ch == ')')
+        //         stk.pop();
+        //     else if (stk.peek() == '[' && ch == ']')
+        //         stk.pop();
+        //     else
+        //         stk.push(ch);
+        // }
+        // if (stk.isEmpty())
+        //     return true;
+        // else
+        //     return false;
+
+        //-----------------Method 2nd--------------------
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(')
+                stk.push(')');
+            else if (ch == '{')
+                stk.push('}');
+            else if (ch == '[')
+                stk.push(']');
+            else {
+                if (stk.isEmpty() || stk.pop() != ch)
+                    return false;
             }
-            if (stk.peek() == '}' || stk.peek() == ']' || stk.peek() == ')')
-                return false;
-            if (stk.peek() == '{' && ch == '}')
-                stk.pop();
-            else if (stk.peek() == '(' && ch == ')')
-                stk.pop();
-            else if (stk.peek() == '[' && ch == ']')
-                stk.pop();
-            else
-                stk.push(ch);
         }
         if (stk.isEmpty())
             return true;
         else
             return false;
-
     }
 }
