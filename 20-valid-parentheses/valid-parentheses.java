f48ac7244cea3e972ac01ee68f8945bcf6b0c2d7
Kaushal Raj <kaushalrajverma2007@gmail.com>
2026-08-11 18:23:04 +0530

    Time: 3 ms (86.07%) | Memory: 43.2 MB (58.06%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,6 +1,29 @@
 class Solution {
     public boolean isValid(String s) {
 
+        // Stack<Character> st = new Stack<>();
+
+        // for (int i = 0; i < s.length(); i++) {
+        //     char ch = s.charAt(i);
+        //     if (ch == '(')
+        //         st.push(')');
+        //     else if (ch == '{')
+        //         st.push('}');
+        //     else if (ch == '[')
+        //         st.push(']');
+        //     else if (st.size() != 0 && st.peek() != ch)
+        //         return false;
+        //         else if( st.size()==0 && (ch==')'||ch=='}'|| ch==']'))
+        //         return false;
+        //     else
+        //         st.pop();
+        // }
+        // if (st.size() != 0)
+        //     return false;
+        // else
+        //     return true;
+
+        //-----------------------------------
         Stack<Character> st = new Stack<>();
 
         for (int i = 0; i < s.length(); i++) {
@@ -11,12 +34,8 @@ class Solution {
                 st.push('}');
             else if (ch == '[')
                 st.push(']');
-            else if (st.size() != 0 && st.peek() != ch)
+            else if (st.size() == 0 || st.pop() != ch)
                 return false;
-                else if( st.size()==0 && (ch==')'||ch=='}'|| ch==']'))
-                return false;
-            else
-                st.pop();
         }
         if (st.size() != 0)
             return false;
