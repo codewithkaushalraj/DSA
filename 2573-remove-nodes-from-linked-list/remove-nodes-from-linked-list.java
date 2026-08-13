21b14512588092c5ed02a38828974e4ff1411cee
Kaushal Raj <kaushalrajverma2007@gmail.com>
2026-08-13 19:20:43 +0530

    Time: 6 ms (92.14%) | Memory: 130.2 MB (87.63%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -17,55 +17,56 @@ class Solution {
     public ListNode removeNodes(ListNode head) {
 
         //------------------(Solved By LinkedList )----------------------
-        // if (head.next == null)
-        //     return head;
+        if (head.next == null)
+            return head;
 
-        // ListNode reverseHead = reverse(head);
-        // ListNode dummy = new ListNode(-1);
-        // ListNode newHead = dummy;
-        // int maxVal = Integer.MIN_VALUE;
-
-        // ListNode temp = reverseHead;
-
-        // while (temp != null) {
-        //     if (temp.val >= maxVal) {
-        //         dummy.next = temp;
-        //         maxVal = temp.val;
-        //         dummy = dummy.next;
-        //     }
-        //     temp = temp.next;
-        // }
-        // dummy.next = null;
-
-        // newHead = reverse(newHead.next);
-
-        // return newHead;
-
-        //------------------(Solved By Stack )----------------
-
-        Stack<ListNode> st = new Stack<>();
-        ListNode temp = head;
-
-        while (temp != null) {
-            if (st.size() != 0 && temp.val > st.peek().val) {
-                while (st.size() != 0 && temp.val > st.peek().val)
-                    st.pop();
-            }
-            st.push(temp);
-            temp = temp.next;
-        }
-
-        Stack<ListNode> temporary = new Stack<>();
-        while (st.size() != 0)
-            temporary.push(st.pop());
-
+        ListNode reverseHead = reverse(head);
         ListNode dummy = new ListNode(-1);
         ListNode newHead = dummy;
-        while (temporary.size() != 0) {
-            dummy.next = temporary.pop();
-            dummy = dummy.next;
+        int maxVal = Integer.MIN_VALUE;
+
+        ListNode temp = reverseHead;
+
+        while (temp != null) {
+            if (temp.val >= maxVal) {
+                dummy.next = temp;
+                maxVal = temp.val;
+                dummy = dummy.next;
+            }
+            temp = temp.next;
         }
-        return newHead.next;
+        dummy.next = null;
+
+        newHead = reverse(newHead.next);
+
+        return newHead;
+
+
+
+        //------------------(Solved By Stack )----------------
+        // Stack<ListNode> st = new Stack<>();
+        // ListNode temp = head;
+
+        // while (temp != null) {
+        //     if (st.size() != 0 && temp.val > st.peek().val) {
+        //         while (st.size() != 0 && temp.val > st.peek().val)
+        //             st.pop();
+        //     }
+        //     st.push(temp);
+        //     temp = temp.next;
+        // }
+
+        // Stack<ListNode> temporary = new Stack<>();
+        // while (st.size() != 0)
+        //     temporary.push(st.pop());
+
+        // ListNode dummy = new ListNode(-1);
+        // ListNode newHead = dummy;
+        // while (temporary.size() != 0) {
+        //     dummy.next = temporary.pop();
+        //     dummy = dummy.next;
+        // }
+        // return newHead.next;
 
     }
 }
