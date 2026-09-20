09177add5f65e460e1a4a4d4a9cf3f5b57814f48
Kaushal Raj <kaushalrajverma2007@gmail.com>
2026-09-20 21:24:24 +0530

    Time: 1 ms (99.98%) | Memory: 45.2 MB (86.24%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.java
+++ 113-path-sum-ii/path-sum-ii.java
@@ -1,18 +1,3 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public void helper(TreeNode root, List<Integer> l, List<List<Integer>> ans, int targetSum, int sum) {
         if (root == null)
@@ -27,12 +12,6 @@ class Solution {
 
         l.add(root.val);
         sum += root.val;
-
-        // if -ve number should come then on that case your answer should be wrong that's by did not add this into a integer case if positive integer's come only then this case will worth it ..
-        // if (sum > targetSum) {
-        //     l.remove(l.size() - 1);
-        //     return;
-        // }
 
         helper(root.left, l, ans, targetSum, sum);
         helper(root.right, l, ans, targetSum, sum);
