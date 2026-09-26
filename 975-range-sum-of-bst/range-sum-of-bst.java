7caf2219b8eefb3ba7ad49d53fa834bfcca2fa8c
Kaushal Raj <kaushalrajverma2007@gmail.com>
2026-09-26 19:56:41 +0530

    Time: 1 ms (40.04%) | Memory: 53.8 MB (64.43%) - LeetSync

---
+++ 975-range-sum-of-bst/range-sum-of-bst.java
@@ -0,0 +1,24 @@
+class Solution {
+    static int sum;
+
+    public void traversal(TreeNode root, int low, int high) {
+
+        if (root == null)
+            return;
+        if (root.val >= low && root.val <= high) {
+            sum += root.val;
+        }
+
+        traversal(root.left, low, high);
+        traversal(root.right, low, high);
+    }
+
+    public int rangeSumBST(TreeNode root, int low, int high) {
+
+        sum = 0;
+
+        traversal(root, low, high);
+        return sum;
+
+    }
+}
